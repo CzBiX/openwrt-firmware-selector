5762b5b465160287e657376a783b4d859fd0ce2c
CzBiX <CzBiX@users.noreply.github.com>
2025-12-01 00:42:19 +0800

    rename logo to banner

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,7 @@ import config from '~/config'
 <template>
   <header class="text-white px-4 bg-[--p-primary-500] flex min-h-16 items-center justify-between">
     <a :href="config.HOME_URL" target="_blank">
-      <img src="/logo.svg" width="180" alt="Logo">
+      <img src="/banner.svg" width="180" alt="Logo">
     </a>
     <LangSelect />
   </header>
